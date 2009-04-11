6a0c3836e5ecd5e930e6fbfe5e28209fa208621b
Markus Prinz <markus.prinz@nuclearsquid.com>
2009-04-11 15:43:18 +0200

    Ignore case when testing against site content

--- test/spec_rack_dick_bar_blocker.rb
+++ test/spec_rack_dick_bar_blocker.rb
@@ -21,8 +21,8 @@ context "Rack::Contrib::DickBarBlocker" do
 
       status.should.equal 200
       headers['Content-Type'].should.equal 'text/html'
-      body.should =~ %r{<title>Say no to the DiggBar</title>}
-      body.should =~ %r{Dear Digg.*Framing Sites is bullshit\.}m
+      body.should =~ %r{<title>Say no to the DiggBar</title>}i
+      body.should =~ %r{Dear Digg.*Framing Sites is bullshit\.}mi
     end
   end
 end
