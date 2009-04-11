f89b545beaf056c879e11e2c86d6f2e10d68b726
Markus Prinz <markus.prinz@nuclearsquid.com>
2009-04-11 15:45:07 +0200

    Update HTML to the one used by Daring Fireball

--- lib/rack/contrib/dick_bar_blocker.rb
+++ lib/rack/contrib/dick_bar_blocker.rb
@@ -10,16 +10,29 @@ module Rack
         # Regex courtesy of John Gruber: http://daringfireball.net/2009/04/how_to_block_the_diggbar
         if referrer && referrer =~ %r{http://digg.com/\w{1,8}/*(\?.*)?$}
           body = <<BODY
-<!DOCTYPE html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN">
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <title>Say no to the DiggBar</title>
-  </head>
-  <body>
-    Dear Digg<br />
-    Framing Sites is bullshit.<br />
-  </body>
+    <head>
+      <meta charset="UTF-8" />
+      <title>Say no to the DiggBar</title>
+    </head>
+    <body>
+        <p>
+            Dear Digg,<br>
+            Framing sites is bullshit.<br>
+            <br>
+            Your pal,<br>
+            —J.G.
+        </p>
+        <p>
+            p.s. Firefox users may enjoy the<br>
+            <a href='http://userscripts.org/scripts/show/45795'>DiggBar Killer script for Greasemonkey</a>.
+        </p>
+        <p>
+            p.p.s. Digg users can disable the DiggBar under<br>
+            My Profile → Settings → <a href='http://digg.com/settings/viewing'>Viewing Preferences</a>.
+        </p>
+    </body>
 </html>
 BODY
 
